a3542df20823aab00b33cf3f29d9568858d6db7c
NicolasAili <58044548+NicolasAili@users.noreply.github.com>
2020-12-23 23:29:53 +0100

    affichage allconcerts

--- allconcerts.php
+++ allconcerts.php
@@ -39,8 +39,10 @@
 			}
 		?>
 		<?php
-			$str = "SELECT * FROM `concert`, `artiste`, `salle` WHERE concert.Nom_artiste = artiste.Nom_artiste AND concert.Nom_salle = salle.Nom_salle";
+			$str = "SELECT id_concert, datec, heure, lien_fb, lien_ticket, concert.nom_artiste, adresse, nom_salle, nom_ville, ville_code_postal, nom_departement, nom_region, nom_pays FROM concert, artiste, salle, ville, departement, region, pays WHERE concert.nom_artiste = artiste.Nom_artiste AND concert.fksalle = salle.id_salle AND salle.id_ville = ville.ville_id AND ville.ville_departement = departement.numero AND departement.id_region = region.id AND region.id_pays = pays.id";
 			$result = mysqli_query($con, $str);
+			$stre = "SELECT id_concert, datec, heure, lien_fb, lien_ticket, concert.nom_artiste, adresse, nom_ext, nom_ville, ville_code_postal, nom_departement, nom_region, nom_pays FROM concert, artiste, exterieur, ville, departement, region, pays WHERE concert.nom_artiste = artiste.Nom_artiste AND concert.fkext = exterieur.id_ext AND exterieur.id_ville = ville.ville_id AND ville.ville_departement = departement.numero AND departement.id_region = region.id AND region.id_pays = pays.id";
+			$resulte = mysqli_query($con, $stre);
 			$admin = 'administateur';
 			//echo $_SESSION['pseudo'];
 			//echo $admin;
@@ -51,33 +53,33 @@
 					{
 						?> 
 						<div class="inwhile"> 
-							<div class="artiste"> <?php echo $row['Nom_artiste'] ?> </div> 
+							<div class="artiste"> <?php echo $row['nom_artiste'] ?> </div> 
 								<div class="dahe">Date et heure</div>
 							<div class="date"> <?php echo  $row['datec'] ?> </div>  
 							<div class="heure"> <?php echo $row['heure'] ?> </div>  
 								<div class="pacp">Pays ville et CP</div>
-							<div class="pays"> <?php echo  $row['Pays'] ?> </div> 
-							<div class="region"> <?php echo  $row['Region'] ?> </div> 
-							<div class="departement"> <?php echo  $row['Departement'] ?> </div> 
-							<div class="ville"> <?php echo $row['Ville'] ?> </div> 
-							<div class="cp"> <?php echo  $row['CP'] ?> </div>
+							<div class="pays"> <?php echo  $row['nom_pays'] ?> </div> 
+							<div class="region"> <?php echo  $row['nom_region'] ?> </div> 
+							<div class="departement"> <?php echo  $row['nom_departement'] ?> </div> 
+							<div class="ville"> <?php echo $row['nom_ville'] ?> </div> 
+							<div class="cp"> <?php echo  $row['ville_code_postal'] ?> </div>
 								<div class="saad">Salle et adresse</div> 
-							<div class="salle"> <?php echo  $row['Nom_salle'] ?> </div> 
+							<div class="salle"> <?php echo  $row['nom_salle'] ?> </div> 
 							<div class="adresse"> <?php echo $row['adresse'] ?> </div> 
 							<div class="saad">Liens relatifs a l'evenement</div>
 							<div class="fb"> <?php echo  $row['lien_fb'] ?> </div> 
 							<div class="ticket"> <?php echo  $row['lien_ticket'] ?> </div> 
 							<form method="post" action="modifconcert.php" class="modif">
-								<input type="hidden" id="idpost" name="idpost" <?php echo 'value="' . $row['ID_concert'] . '"' ?> > 
-								<input type="hidden" id="artistepost" name="artistepost" <?php echo 'value="' . $row['Nom_artiste'] . '"' ?> > 
+								<input type="hidden" id="idpost" name="idpost" <?php echo 'value="' . $row['id_concert'] . '"' ?> > 
+								<input type="hidden" id="artistepost" name="artistepost" <?php echo 'value="' . $row['nom_artiste'] . '"' ?> > 
 								<input type="hidden" id="datepost" name="datepost" <?php echo 'value="' . $row['datec'] . '"' ?> > 
 								<input type="hidden" id="heurepost" name="heurepost" <?php echo 'value="' . $row['heure'] . '"' ?> > 
-								<input type="hidden" id="payspost" name="payspost" <?php echo 'value="' . $row['Pays'] . '"' ?> > 
-								<input type="hidden" id="regionpost" name="regionpost" <?php echo 'value="' . $row['Region'] . '"' ?> > 
-								<input type="hidden" id="departementpost" name="departementpost" <?php echo 'value="' . $row['Departement'] . '"' ?> > 
-								<input type="hidden" id="villepost" name="villepost" <?php echo 'value="' . $row['Ville'] . '"' ?> > 
-								<input type="hidden" id="cppost" name="cppost" <?php echo 'value="' . $row['CP'] . '"' ?> > 
-								<input type="hidden" id="sallepost" name="sallepost" <?php echo 'value="' . $row['Nom_salle'] . '"' ?> > 
+								<input type="hidden" id="payspost" name="payspost" <?php echo 'value="' . $row['nom_pays'] . '"' ?> > 
+								<input type="hidden" id="regionpost" name="regionpost" <?php echo 'value="' . $row['nom_region'] . '"' ?> > 
+								<input type="hidden" id="departementpost" name="departementpost" <?php echo 'value="' . $row['nom_departement'] . '"' ?> > 
+								<input type="hidden" id="villepost" name="villepost" <?php echo 'value="' . $row['nom_ville'] . '"' ?> > 
+								<input type="hidden" id="cppost" name="cppost" <?php echo 'value="' . $row['ville_code_postal'] . '"' ?> > 
+								<input type="hidden" id="sallepost" name="sallepost" <?php echo 'value="' . $row['nom_salle'] . '"' ?> > 
 								<input type="hidden" id="adressepost" name="adressepost" <?php echo 'value="' . $row['adresse'] . '"' ?> > 
 								<input type="hidden" id="fbpost" name="fbpost" <?php echo 'value="' . $row['lien_fb'] . '"' ?> > 
 								<input type="hidden" id="ticketpost" name="ticketpost" <?php echo 'value="' . $row['lien_ticket'] . '"' ?> > 
@@ -95,7 +97,60 @@
 						</div>
 					<?php
 		 			}
-		 			?>			
+		 			?>
+		 		<br>
+		 		Concerts extérieurs
+		 		<br>
+	 			<?php
+				while($row = mysqli_fetch_array($resulte)) 
+				{
+					?> 
+					<div class="inwhile"> 
+						<div class="artiste"> <?php echo $row['nom_artiste'] ?> </div> 
+							<div class="dahe">Date et heure</div>
+						<div class="date"> <?php echo  $row['datec'] ?> </div>  
+						<div class="heure"> <?php echo $row['heure'] ?> </div>  
+							<div class="pacp">Pays ville et CP</div>
+						<div class="pays"> <?php echo  $row['nom_pays'] ?> </div> 
+						<div class="region"> <?php echo  $row['nom_region'] ?> </div> 
+						<div class="departement"> <?php echo  $row['nom_departement'] ?> </div> 
+						<div class="ville"> <?php echo $row['nom_ville'] ?> </div> 
+						<div class="cp"> <?php echo  $row['ville_code_postal'] ?> </div>
+							<div class="saad">Lieu et adresse</div> 
+						<div class="salle"> <?php echo  $row['nom_ext'] ?> </div> 
+						<div class="adresse"> <?php echo $row['adresse'] ?> </div> 
+						<div class="saad">Liens relatifs a l'evenement</div>
+						<div class="fb"> <?php echo  $row['lien_fb'] ?> </div> 
+						<div class="ticket"> <?php echo  $row['lien_ticket'] ?> </div> 
+						<form method="post" action="modifconcert.php" class="modif">
+							<input type="hidden" id="idpost" name="idpost" <?php echo 'value="' . $row['id_concert'] . '"' ?> > 
+							<input type="hidden" id="artistepost" name="artistepost" <?php echo 'value="' . $row['nom_artiste'] . '"' ?> > 
+							<input type="hidden" id="datepost" name="datepost" <?php echo 'value="' . $row['datec'] . '"' ?> > 
+							<input type="hidden" id="heurepost" name="heurepost" <?php echo 'value="' . $row['heure'] . '"' ?> > 
+							<input type="hidden" id="payspost" name="payspost" <?php echo 'value="' . $row['nom_pays'] . '"' ?> > 
+							<input type="hidden" id="regionpost" name="regionpost" <?php echo 'value="' . $row['nom_region'] . '"' ?> > 
+							<input type="hidden" id="departementpost" name="departementpost" <?php echo 'value="' . $row['nom_departement'] . '"' ?> > 
+							<input type="hidden" id="villepost" name="villepost" <?php echo 'value="' . $row['nom_ville'] . '"' ?> > 
+							<input type="hidden" id="cppost" name="cppost" <?php echo 'value="' . $row['ville_code_postal'] . '"' ?> > 
+							<input type="hidden" id="sallepost" name="sallepost" <?php echo 'value="' . $row['nom_ext'] . '"' ?> > 
+							<input type="hidden" id="adressepost" name="adressepost" <?php echo 'value="' . $row['adresse'] . '"' ?> > 
+							<input type="hidden" id="fbpost" name="fbpost" <?php echo 'value="' . $row['lien_fb'] . '"' ?> > 
+							<input type="hidden" id="ticketpost" name="ticketpost" <?php echo 'value="' . $row['lien_ticket'] . '"' ?> > 
+							<style type="text/css">
+								#footer 
+								{
+								  display:flex;
+								}
+							</style>
+							<div id="footer">
+								<input id="modifier" type="submit" name="modsuppr" value="Modifier"> 
+								<input id="supprimer" type="submit" name="modsuppr" value="Supprimer"> 
+							</div>
+						</form>
+					</div>
+				<?php
+	 			}
+	 			?>
 			</div>
 	</body>
 	<?php include('footer.html'); ?>

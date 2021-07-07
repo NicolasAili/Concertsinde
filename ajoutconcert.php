7991aa636aef6900bdf48da69f4d4164dcb006f7
NicolasAili <58044548+NicolasAili@users.noreply.github.com>
2021-07-07 23:45:28 +0200

    uncheck bouton int ext refresh

--- ajoutconcert.php
+++ ajoutconcert.php
@@ -142,5 +142,11 @@
 		$(document).ready(function(){
     	$("#divSchedule").dialog({show: "slide", modal: true, autoOpen: false, width: 500});
     	});
+
+    	$( document ).ready(function() 
+    	{
+    	$("#int").prop("checked", false);
+    	$("#ext").prop("checked", false);
+    	});
     </script>
 </html>

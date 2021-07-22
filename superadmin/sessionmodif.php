ce47484ebc8e7a9d3db909688765995812ef939b
NicolasAili <58044548+NicolasAili@users.noreply.github.com>
2021-07-22 20:26:21 +0200

    gestion des sessions

---
+++ superadmin/sessionmodif.php
@@ -0,0 +1,33 @@
+<?php
+require('../php/database.php');
+
+$action = $_POST['modsuppr'];
+
+if($action == 'valider')
+{
+	$idcheck = $_POST['idcheck'];
+	$sql = "UPDATE session SET actif = 0 WHERE actif = 1 ";
+	$query = mysqli_query($con ,$sql);
+	$sql = "UPDATE session SET actif = 1 WHERE id = '$idcheck' ";
+	$query = mysqli_query($con ,$sql);
+	$sql = "UPDATE utilisateur SET points_session = 0";
+	$query = mysqli_query($con ,$sql);
+}
+
+
+
+
+else if($action == 'ajout_session')
+{
+	$date_debut = $_POST['date_debut'];
+	$date_fin = $_POST['date_fin'];
+	$sql = "INSERT INTO session (date_debut, date_fin) VALUES ('$date_debut', '$date_fin')";
+	$query = mysqli_query($con ,$sql);
+}
+
+else
+{
+	echo "erreur";
+}
+
+header("Location: /accueil.php");
--- superadmin/sessions.php
+++ superadmin/sessions.php
@@ -12,9 +12,10 @@
 		<link rel="stylesheet" type="text/css" href="css/header.css" media="screen" />	
 		<link rel="stylesheet" type="text/css" href="css/body/allconcerts.css" media="screen" />
 		<link rel="stylesheet" type="text/css" href="css/footer.css" media="screen" />	
-		<script type="text/javascript" src="./jquery/jquery.min.js"></script>
-		<script type="text/javascript" src="./jquery/jquery-ui.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="./jquery/jquery-ui.css" media="screen" />		
+		<script type="text/javascript" src="../jquery/jquery.min.js"></script>
+		<script type="text/javascript" src="../jquery/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="../js/scriptform.js"></script> 
+		<link rel="stylesheet" type="text/css" href="../jquery/jquery-ui.css" media="screen" />		
 		<meta name="Author" content="BUSQUET_TOURNU" />
 		<meta name="Keywords" content="ConcertAll" />
 		<meta name="Description" content="Recap" />
@@ -28,15 +29,50 @@
 		$row = mysqli_fetch_array($result);
 		if($row['admin'] == 2)
 		{
-			echo "succès";
+			$sql = "SELECT id, date_debut, date_fin, actif FROM session ORDER BY id DESC";
+			$query = mysqli_query($con, $sql);
+			?>
+
+			<h2> Ajouter une session </h2>
+			<br>
+			<form method="post" id="connect" action="sessionmodif.php">
+				<input type="date" name="date_debut" placeholder="Saisir debut session " id="date_debut" required>
+				<input type="date" name="date_fin" placeholder="Saisir fin session " id="date_fin" required>
+				<input type="submit" value="ajout_session" class="ajout_session" name="modsuppr" href="">
+			</form>
+			<br>
+			<br>
+			<br>
+			<table>
+	    		<caption>Utilisateurs</caption>
+	    		<tr>
+	    		<th scope="col">Num</th>
+		        <th scope="col">Du</th>
+		        <th scope="col">Au</th>
+		        <th scope="col">Actif</th>
+   				</tr><?php
+   				while($row = mysqli_fetch_array($query))
+				{
+					?>
+					<form method="post" id="connect" action="sessionmodif.php">
+						<tr>
+				        <th scope="row"><?php echo $row['id']; ?></th>
+				        <td><?php echo  $row['date_debut']; ?> </td>
+				        <td><?php echo  $row['date_fin']; ?> </td>
+				        <td><input type="checkbox" class="actif" name="actif" <?php if($row['actif'] == 1){echo "checked "; echo "disabled";} else{echo "required";} ?> > </td>
+				        <input type="hidden" class="idcheck" name="idcheck" <?php echo 'value="' . $row['id'] . '"' ?> >
+				        <?php if($row['actif'] == 0){?><td><input type="submit" value="valider" class="valider" name="modsuppr" href=""></td><?php } ?>
+		    			</tr>
+		    		</form>
+	    		<?php
+				}
+				?>
+			</table><?php
 		}
 		else
 		{
 			echo "accès non autorisé";
 		}
 		?>
-
-
-
 	</body>
 </html>

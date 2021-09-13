b1cf45b20e5824b44242f12afb02ecbf6cd9d48d
NicolasAili <58044548+NicolasAili@users.noreply.github.com>
2021-09-13 11:21:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -42,8 +42,9 @@
 		<br>
 		<br>
 		<br><br>
-
+		hééééééééééhoooooooooooooo
 		<?php
+		echo "coucouc";
 			$pseudo = $_SESSION['pseudo'];
 			$requestpseudo = "SELECT id_user FROM utilisateur WHERE pseudo = '$pseudo'";
 			$query = mysqli_query($con, $requestpseudo);
@@ -106,7 +107,7 @@
 			</div>	
 		</div>
 	</body>
-	<script type="text/javascript" src="./js/scrollnav.js"></script> 
+	<!--<script type="text/javascript" src="./js/scrollnav.js"></script> -->
 	<?php include('footer.html'); ?>
 </html>
 

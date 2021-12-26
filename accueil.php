4f17bcd4729a2c00ed3d45837578c8b38c58e31e
NicolasAili <58044548+NicolasAili@users.noreply.github.com>
2021-12-26 03:25:58 +0100

    accueil

--- accueil.php
+++ accueil.php
@@ -32,12 +32,16 @@ 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
 	<body>
 		<header>
 			<h1> <a href="accueil.php" id="logo"> Arpenid <div id="com">.com</div></a> </h1>
-			<a href="presentation.php" class="li"><div class="txtli">Fonctionnement</div></a>
-			<a href="news.php" class="li"><div class="txtli">Actualités</div></a>
-			<a href="artistes.php" class="li"><div class="txtli">Artistes</div></a>
-			<a href="allconcerts.php" class="li"><div class="txtli">Concerts</div></a>
-			<a href="classement.php" class="li"><div class="txtli">Contributeurs</div></a>
-			<a href="./connexion.php" class="spacelink" role="button"> Connexion </a>
+			<div id="corps">
+				<a href="presentation.php" class="li"><div class="txtli">Fonctionnement</div></a>
+				<a href="news.php" class="li"><div class="txtli">Actualités</div></a>
+				<a href="artistes.php" class="li"><div class="txtli">Artistes</div></a>
+				<a href="allconcerts.php" class="li"><div class="txtli">Concerts</div></a>
+				<a href="classement.php" class="li"><div class="txtli">Contributeurs</div></a>
+			</div>
+			<div id="cnx">
+				<a href="./connexion.php" class="spacelink" role="button"> <span> Connexion </span> </a>
+			</div>
 		</header>
 		<div id="main">
 			<?php
@@ -52,12 +56,22 @@ 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
 				<h1> Arpenid.com </h1>
 				<h2> Le rap indé, par le public, pour le public</h2>
 				<a class="ajoutconcert" href="ajoutconcert.php"> Ajouter un concert </a>
+				<a class="allconcerts" href="allconcerts.php"> Liste des concerts </a>
 			</div>
 			<div id="bottom">
-				<span> Ajoutez des concerts </span>
-				<span> Modifiez des concerts </span>
-				<span> Gagnez des points </span>
-				<span> Obtenez des récompenses </span>
+				<div id="bottomun">
+					<span> Participez à l'activité du site </span>
+					<img src="image/transition.png" width="50" height="50">
+					<span> Gagnez des points </span>
+					<img src="image/transition.png" width="50" height="50">
+					<span> Obtenez des récompenses </span>
+				</div>
+				<!--<div id="bottomdeux">
+					<span> Mais surtout...</span>
+				</div>
+				<div id="bottomtrois">
+					<span> Soutenez </span> la scène indépendante, <span> supportez </span> les artistes et <span> aidez </span> le public
+				</div>-->
 			</div>
 		</div>
 	</body> 
@@ -71,9 +85,11 @@ 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
 		var viewboxh = height/10;
 		$('svg').css('height', height);
 		$('svg').css('width', width);
+		$('header').css('width', width);
 		$('#main').css('height', height);
 		$('#main').css('width', width);
 		$('svg').attr('viewBox', "0 0 " + viewboxw + " " + viewboxh);
+		console.log($('svg').css('width'));
     });
 </script>
 
--- contenu/reseaux.php
+++ contenu/reseaux.php
@@ -9,14 +9,10 @@
 	CSS : oui
 */
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-	<link rel="stylesheet" type="text/css" href="css/reseaux.css">
-	<div id="reseaux">
-		<a href="https://facebook.com" class="imgreseaux"><img class="imgfb" src="./image/facebook.png"></a>
-		<a href="https://instagram.com" class="imgreseaux"><img class="imgig" src="./image/instagram.png"></a>
-		<a href="https://discord.com" class="imgreseaux"><img class="imgdiscord" src="./image/discord.png"></a>
-		<a href="contact.php" class="imgreseaux"><img class="contact" src="./image/bulle.png"></a>
-	</div>
-</html>
+<link rel="stylesheet" type="text/css" href="css/reseaux.css">
+<div id="reseaux">
+	<a href="https://facebook.com" class="imgreseaux"><img class="imgfb" src="./image/facebook.png"></a>
+	<a href="https://instagram.com" class="imgreseaux"><img class="imgig" src="./image/instagram.png"></a>
+	<a href="https://discord.com" class="imgreseaux"><img class="imgdiscord" src="./image/discord.png"></a>
+	<a href="contact.php" class="imgreseaux"><img class="contact" src="./image/bulle.png"></a>
+</div>
